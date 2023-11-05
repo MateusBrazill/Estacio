b9d09fd1ab06960f64a315118548e688840fc273
Mateus Brazil <142843420+MateusBrazill@users.noreply.github.com>
2023-11-05 19:50:47 -0300

    Create q1.c
    Questão 1

---
+++ q1.c
@@ -0,0 +1,48 @@
+//Aluno: Mateus Brazil de Oliveira
+//Matricula: 202003447031
+
+#include<LiquidCrystal.h>
+
+#define tempSensor A1
+#define soilSensor A2
+
+const int rs = 9, en = 8, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+
+void setup()
+{
+	pinMode(tempSensor, INPUT);
+  	pinMode(soilSensor, INPUT);
+  	Serial.begin(9600);
+  	lcd.begin(16,2);
+}
+
+void loop()
+{
+	double temperatureRaw = 0, parsedTemp = 0, soilSensorRaw = 0, parsedSoilSensor = 0;
+    
+  	temperatureRaw = analogRead(tempSensor); 
+  	soilSensorRaw = analogRead(soilSensor);
+   	
+  	parsedTemp = (temperatureRaw*0.004882814-0.5)*100;
+  	parsedSoilSensor = ((soilSensorRaw/1024.00)*100);
+	
+  	//Serial print
+  	Serial.print("Temp C:");
+  	Serial.println(parsedTemp);
+  	Serial.print("Water %:");
+  	Serial.println(parsedSoilSensor);
+  	
+  	//LCD print
+    lcd.setCursor(0,0);
+	lcd.print("Temp: ");
+  	lcd.print(parsedTemp);
+	lcd.print(" C");
+  	lcd.setCursor(0,1);
+  	lcd.print("Water %:");
+  	lcd.print(parsedSoilSensor);
+  
+  	delay(1000);
+  	lcd.clear();
+}
